84a047f0c11dd6d951ebde9027496e7f309f564b
phanispeaks <phanibonthu7@gmail.com>
2020-04-28 16:58:30 +0530

    Updates

---
+++ precache-manifest.3e575d250def233af300f2de23563305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ea071553c58be485d7d36159634529",
+    "url": "/phani-react/index.html"
+  },
+  {
+    "revision": "38e5e158fbb012d65b84",
+    "url": "/phani-react/static/css/main.c6be87ae.chunk.css"
+  },
+  {
+    "revision": "9da0c747093fd2d4164a",
+    "url": "/phani-react/static/js/2.789b5561.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phani-react/static/js/2.789b5561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e5e158fbb012d65b84",
+    "url": "/phani-react/static/js/main.e08e4461.chunk.js"
+  },
+  {
+    "revision": "f25f271c5538a93f2142",
+    "url": "/phani-react/static/js/runtime-main.d59749e5.js"
+  }
+]);
